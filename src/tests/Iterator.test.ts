f9d50654824d01ac515de2907307e6788a534894
Cristian Rojas <cristian.rojas@live.fr>
2025-05-13 22:24:16 +0200

    iterate() iterates N time on never fullfilling condition

---
+++ src/tests/Iterator.test.ts
@@ -0,0 +1,25 @@
+import { describe, it, expect } from "vitest";
+
+class Iterator {
+  public count = 0;
+
+  async iterate(
+    nTimes: number,
+    until: () => boolean,
+    action: () => void | Promise<void>,
+  ): Promise<void> {
+    while (this.count < nTimes && !until()) {
+      await action();
+      this.count++;
+    }
+  }
+}
+
+describe("Iterator", () => {
+  it("iterates N times", async () => {
+    const sut = new Iterator();
+    const neverFulfillsCondition = () => false;
+    await sut.iterate(5, neverFulfillsCondition, () => {});
+    expect(sut.count).toBe(5);
+  });
+});
