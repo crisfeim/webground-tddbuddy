b701d25258d4ef558df8ab9b05f8bf411a523c5a
Cristian Rojas <cristian.rojas@live.fr>
2025-05-14 16:07:14 +0200

    Imporve JSRunner success test

--- src/tests/Infra/JSRunner.test.ts
+++ src/tests/Infra/JSRunner.test.ts
@@ -9,10 +9,32 @@ describe("run", () => {
     expect(output.exitCode).toBe(1);
   });
 
+  const codeToRun = `
+    function Adder(a, b) {
+      return {
+        result: a + b
+      };
+    }
+
+    function test_adder() {
+      let sut = Adder(1, 3);
+      console.assert(sut.result === 4, "Expected 1 + 3 to be 4");
+
+      sut = Adder(3, 4);
+      console.assert(sut.result === 7, "Expected 3 + 4 to be 7");
+
+      sut = Adder(5, 4);
+      console.assert(sut.result === 9, "Expected 5 + 4 to be 9");
+    }
+
+    test_adder();
+  `;
   it("delviers exitCode 0 on run success", () => {
     const sut = JSRunner;
     const codeWithNoError = "console.log('hello world')";
-    const output = sut(codeWithNoError);
+    const output = sut(codeToRun);
+
+    expect(output.stderr).toBe("");
     expect(output.exitCode).toBe(0);
   });
 });
